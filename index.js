1f27a75d3d7f595ce2451e40e0254c93cbea9f55
Katerina Thomova <113352240+KacenkaThomova@users.noreply.github.com>
2023-02-06 12:13:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* tady bude tvůj kód */
+console.log('JavaScript ve stránce funguje');
+
+const movieList = document.querySelector ('#movies')
+
+const showMovies = (items) => {
+    data.forEach((item) => {
+        
+       /* movieList.innerHTML += `
+        <div class="movie">
+        <img class="movie__img" 
+            src="${posterUrl}" 
+            alt="Vykoupení z věznice Shawshank">
+        <h2 class="movie__title"> <a href=${url}>${title}</a></h2>
+        <p class="movie__year">${year}</p>
+        <p class="movie__genre">${genres}</p>
+        </div> `*/
+        console.log(title)
+    });
+}
+
+fetch ('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+.then((resp) => resp.json())
+.then((data) => showMovies(data))
